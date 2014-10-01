5e7a1dc7a0616d8909bf2014c9e2176a57a0b8c6
Kári Bertilsson <karibertils@gmail.com>
2014-10-01 01:12:10 +0000

    Reading data from mongodb

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var jobModel = require('./models/job');
 
 var app = express();
 
@@ -7,6 +8,12 @@ app.set('views', __dirname);
 app.set('view engine', 'jade');
 
 app.use(express.static(__dirname + '/public'));
+
+app.get('/api/jobs', function(req, res) {
+	mongoose.model('Job').find({}).exec(function(error, collection) {
+		res.send(collection);
+	})
+})
 
 app.get('*', function(req, res) {
 	res.render('index');
@@ -17,6 +24,7 @@ mongoose.connect('mongodb://localhost/jobfinder');
 var mongodb = mongoose.connection;
 mongodb.once('open', function() {
 	console.log('connected to mongodb successfully!');
+	jobModel.seedJobs();
 });
 
 app.listen(3000);
