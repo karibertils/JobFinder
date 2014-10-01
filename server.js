eef5f69174743d2e0bb93dc09842ac35b3ae68e6
Kári Bertilsson <karibertils@gmail.com>
2014-10-01 00:32:23 +0000

    Added Angular

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var app = express();
 app.set('views', __dirname);
 app.set('view engine', 'jade');
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('*', function(req, res) {
 	res.render('index');
 });
