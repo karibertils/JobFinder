0f67ecb0ec17da848f00dea5e269dda22d0102f0
Kári Bertilsson <karibertils@gmail.com>
2014-10-04 21:32:51 +0000

    Added new test

--- test/jobs-service-spec.js
+++ test/jobs-service-spec.js
@@ -1,4 +1,16 @@
+var express = require('express');
+var app = express();
 var expect = require("chai").expect;
+var request = require("supertest");
+
+var dataSavedJob;
+
+var db = {
+	saveJob: function(job) {
+		dataSavedJob = job;
+	}
+};
+var jobService = require("../jobs-service.js")(db, app);
 
 describe("save jobs", function() {
 	it("should validate that title is greater than 4 characters");
@@ -6,7 +18,14 @@ describe("save jobs", function() {
 	it("should validate that description is greater than 4 characters");
 	it("should validate that description is less than 250 characters");
 
-	it("should pass the job to the database save");
+	var newJob = {title:'Cook', description:'You will be making bagels'};
+
+	it("should pass the job to the database save", function(done) {
+		request(app).post('/api/jobs').send(newJob).end(function(err, res) {
+			expect(dataSavedJob).to.deep.equal(newJob);
+			done();
+		});
+	});
 	it("should return a status of 200 to the front end if the database saved");
 	it("should return a job with an id");
 	it("should return an error if the database failed");
